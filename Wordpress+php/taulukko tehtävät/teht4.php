59451096d66f920a4d13f109ac43fcc51c49b272
Aada-koodari <aada.rauhala@edu.salpaus.fi>
2024-11-18 09:31:03 +0200

    taulukko tehtävät

--- Wordpress+php/taulukko tehtävät/teht4.php
+++ Wordpress+php/taulukko tehtävät/teht4.php
@@ -10,11 +10,23 @@
 </html>
 
 <?php
-$oppilaat = array("Maija", "Teijo", "Anna", "Miro", "Saana");
-sort($oppilaat);
-foreach ($oppilaat as $oppilas)
-{
-    echo $oppilas . "<br>";
-}
+$maat = array(
+    array("Index" => "Index", "Country" => "Country", "Capital" => "Capital", "Population" => "Population"),
+    array("Index" => 0, "Country" => "Suomi", "Capital" => "Helsinki", "Population" => "1 300 000"),
+    array("Index" => 1, "Country" => "Ruotsi", "Capital" => "Stockholm", "Population" => "1 700 000"),
+    array("Index" => 2, "Country" => "Norja", "Capital" => "Oslo", "Population" => "1 100 000"),
+    array("Index" => 3, "Country" => "Tanska", "Capital" => "Copenhagen", "Population" => "1 390 000"),
+    array("Index" => 4, "Country" => "Islanti", "Capital" => "Reykjavik", "Population" => "400 000"),
+);
 
+foreach ($maat as $maaTiedot)
+{
+    echo $maaTiedot["Index"] ." ". $maaTiedot["Country"] ." ". $maaTiedot["Capital"] ." ". $maaTiedot["Population"] . "<br>";
+}
+/*Index	Country	Capital	Population
+0	Suomi	Helsinki	1 300 000
+1	Ruotsi	Stockholm	1 700 000
+2	Norja	Oslo	1 100 000
+3	Tanska	Copenhagen	1 390 000
+4	Islanti	Reykjavik	400 000*/
 ?>
